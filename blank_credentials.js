c7eff5a396335545c60fefc29de35b38c29af7b7
Nick Terry <j.nick.terry@gmail.com>
2017-01-27 09:03:49 -0800

    refined matrix

---
+++ blank_credentials.js
@@ -0,0 +1,5 @@
+module.exports = {
+  email = 'j.nick.terry@gmail.com',
+  password: '',
+  branch: 'default',
+};
--- src/matrix.js
+++ src/matrix.js
@@ -1,9 +1,11 @@
-
-let cache = {};
-
-Room.prototype.calcMatrix = function(name, matrix) {
+function calcMatrix(name, matrix) {
   const room = Game.rooms[name];
+  Memory.matrix = Memory.matrix || {};
   if(!room) {
+    let cached = Memory.matrix[name];
+    if(cached) {
+      return PathFinder.CostMatrix.deserialize(cached);
+    }
     return;
   }
 
@@ -12,29 +14,22 @@ Room.prototype.calcMatrix = function(name, matrix) {
       return this.matrix;
     }
 
-    let cached = cache[name];
-    if(cached) {
-      this.matrix = PathFinder.CostMatrix.deserialize(cached);
-      return this.matrix;
-    }
-
-    this.matrix = matrix.clone()
+    this.matrix = matrix;
     const towers = room.Structures(STRUCTURE_TOWER);
     for(let tower of towers) {
       for(let x = 0; x<50; x++){
         for(let y = 0; y<50; y++){
           const pos = new RoomPosition(x, y, name);
           const range = pos.getRangeTo(tower);
-          const extra = Math.min(0, 20 - range)
+          const extra = Math.min(0, 20 - range);
           this.matrix.set(x, y, this.matrix.get(x, y) + 10 + extra);
         } 
       } 
     }
-    cache[name] = m;
-    return m;
+    Memory.matrix[name] = this.matrix.serialize();
   }
 };
 
 module.exports = {
-  upkeep: upkeep,
+  calcMatrix: calcMatrix,
 };
